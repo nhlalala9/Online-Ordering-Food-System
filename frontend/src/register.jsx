33351c53db98f44801b41a6088cc3b7a76b813e5
Oratile <oratile.mabote@dadevs.co.za>
2023-01-10 15:25:26 +0200

    wqwerty

---
+++ frontend/src/register.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+    const[username, setUsername] = useState("username");
+
+  return (
+    <div></div>
+  )
+}
+
+export default Register
